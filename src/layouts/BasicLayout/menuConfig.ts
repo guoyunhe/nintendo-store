1df6fc1165e47047428b84c548ae78f9125bffbd
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-04-01 17:35:22 +0800

    feat: nintendo games

---
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -0,0 +1,18 @@
+const headerMenuConfig = [];
+
+const asideMenuConfig = [
+  {
+    name: 'Games',
+    path: '/',
+  },
+  {
+    name: 'PlayStation Games',
+    path: '/playstation',
+  },
+  {
+    name: 'XBox Games',
+    path: '/xbox',
+  },
+];
+
+export { headerMenuConfig, asideMenuConfig };
---
+++ src/pages/games/index.tsx
@@ -0,0 +1,34 @@
+import { Button, Card, Grid } from '@alifd/next';
+import React from 'react';
+import games from './games';
+
+const prefix =
+  'https://assets.nintendo.com/image/upload/ar_16:9,b_auto,c_pad,dpr_2.0,f_auto,q_auto,w_300/b_rgb:ffffff/v1';
+
+export default function Games() {
+  return (
+    <div>
+      <Grid.Row gutter={30} wrap>
+        {games.map((game) => (
+          <Grid.Col xs={6} key={game.objectID}>
+            <Card free style={{ marginBottom: 30 }}>
+              <Card.Header title={game.title} />
+              <Card.Content>
+                <img
+                  src={`${prefix}/${game.productImage}`}
+                  width="400"
+                  height="225"
+                  style={{ width: '100%', height: 'auto' }}
+                />
+                <div>{game.description}</div>
+              </Card.Content>
+              <Card.Actions>
+                <Button type="primary">Buy</Button>
+              </Card.Actions>
+            </Card>
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+    </div>
+  );
+}
