5dcb8a4a8870d94b5eaeac4c770305e98cfd914e
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-04-02 10:03:23 +0800

    feat: submodules

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { IRouterConfig, lazy } from 'ice';
 import BasicLayout from '@/layouts/BasicLayout';
 
 const Games = lazy(() => import('@/pages/games'));
+const PlayStationGames = lazy(() => import('@/modules/playstation-store/src/pages/games'));
+const XboxGames = lazy(() => import('@/modules/xbox-store/src/pages/games'));
+
 const routerConfig: IRouterConfig[] = [
   {
     path: '/',
@@ -11,6 +14,16 @@ const routerConfig: IRouterConfig[] = [
         path: '/',
         exact: true,
         component: Games,
+      },
+      {
+        path: '/playstation',
+        exact: true,
+        component: PlayStationGames,
+      },
+      {
+        path: '/xbox',
+        exact: true,
+        component: XboxGames,
       },
     ],
   },
